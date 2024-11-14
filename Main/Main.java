277c2a349f81eca432df782d0e70c45cb5a9e28a
Jonatan2106 <if-23015@students.ithb.ac.id>
2024-11-14 16:08:53 +0700

    .

--- Main/Main.java
+++ Main/Main.java
@@ -4,7 +4,6 @@ import Controller.TicketController;
 import View.MainMenu;
 
 public class Main {
-    // Asumsi Method di diagram ada di controller
     public static void main(String[] args) {
         TicketController controller = new TicketController();
         MainMenu menu = new MainMenu(controller);
